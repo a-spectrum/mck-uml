4481be81eaba9881d8119eea307fd5a05d88b8eb
a-spectrum <maisie.c.vdhammen@gmail.com>
2019-05-04 10:51:35 +0200

    Updates

---
+++ precache-manifest.ac740cf91d690993e440b1dc1999676e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96a733e63d27d13886fea7beef6621bf",
+    "url": "/mck-uml/index.html"
+  },
+  {
+    "revision": "7f0738500552bc28546a",
+    "url": "/mck-uml/static/css/main.a93bf4ac.chunk.css"
+  },
+  {
+    "revision": "f18961b46226ec3e725e",
+    "url": "/mck-uml/static/js/2.6746c075.chunk.js"
+  },
+  {
+    "revision": "7f0738500552bc28546a",
+    "url": "/mck-uml/static/js/main.d569dc20.chunk.js"
+  },
+  {
+    "revision": "d961739bae07bc1c449e",
+    "url": "/mck-uml/static/js/runtime~main.e78d8382.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/mck-uml/static/media/logo.ee7cd8ed.svg"
+  }
+]);
